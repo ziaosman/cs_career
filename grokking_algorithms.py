dc86d2aadfd1c8d7ee3e31cd5fe8018d99a135e1
Zia <zia@Zias-MacBook-Air.local>
2022-11-06 11:26:21 +0000

    initial commit

---
+++ grokking_algorithms.py
@@ -0,0 +1,21 @@
+#binary search
+def binary_search(list, item):
+    low = 0
+    high = len(list)-1
+    
+    while low <= high:
+        mid = round((low+high)/2)
+        guess = list[mid]
+
+        if guess == item:
+            return mid
+
+        if guess < item:
+            low = mid + 1
+        #guess is larger than the item
+        else: 
+            high = mid - 1
+    return None
+    
+my_list = [1,3,5,7,9]
+print (binary_search(my_list, 5))
